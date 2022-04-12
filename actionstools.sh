1b68fd45bd73270c1fc4833cb8f692de3e07a1e2
ZzCry <60110836+ZzCry12@users.noreply.github.com>
2022-04-12 21:53:16 +0200

    Added FilesystemScript
    Filesystem Script added

--- actionstools.sh
+++ actionstools.sh
@@ -11,12 +11,31 @@ case $Action
   1 )
     #launch antivirus tools
     # ...
+    echo "Sorry, we're working on it"
+    exit 1
     ;;
 
   2 )
     #Analyse a directory
     #Check permissions
-    # 
+    ##### Team Rocket
+    ##### Dario Rata Script
+    
+#
+Dir=$1
+ls $Dir -1 > buffer.tmp
+while read Element
+do
+    [ -d "$Dir/$Element" ] && echo "$Element is a Directoy"
+    [ -f "$Dir/$Element" ] && echo "$Element is a Filesystem"
+    [ -r "$Dir/$Element" ] && echo "You have writing permissions" || "You don't have writing permission"
+    
+done < buffer.tmp
+
+rm buffer.tmp
+
+exit 1
+
     ;;
   3 )
     #Make a back up with tar
@@ -28,7 +47,7 @@ case $Action
     echo "ERROR to pass a parameter."
     echo "Reminder that all required files will be copied to /home/\$USER/backup/project_backup."
     echo "USAGE: ./backup.sh LOGFILE DIRECTORY-TO-BACKUP"
-    exit 255;
+    exit 1;
   fi
 
 
@@ -39,7 +58,7 @@ case $Action
     rm -rf /home/$USER/backup/project_backup
     rm -f $LOG
     echo "Received Ctrl+C"
-    exit 255
+    exit 1
   }
 
   trap ctrlc SIGINT
